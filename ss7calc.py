54bbd127419e96318529f414191db08e643c944c
ownport <ownport@gmail.com>
2012-11-09 09:46:06 +0200

    added format 6-6-2

--- ss7calc.py
+++ ss7calc.py
@@ -18,6 +18,8 @@ help_message = '''Usage:
     displays this message
 "-3", "--383"
     set Signaling Point Code as 3-8-3 Format (Recommended, used by ITU)
+"-6", "--662"
+   set Signaling Point Code as 6-6-2 Format
 "-5", "--545"
    set Signaling Point Code as 5-4-5 Format
 "-i", "--int"
@@ -40,6 +42,7 @@ Format      : Unknown
 Hex format  : 3039
 5-4-5 Format: 24-1-25
 3-8-3 Format: 6-7-1
+6-6-2 Format: 48-14-1
 
 For more information:
 http://en.wikipedia.org/wiki/Point_code
@@ -113,6 +116,28 @@ class SPC():
                 return
             self.spc = a*2**9 + b*2**5 + c
 
+    def set_662(self, spc662):
+        """
+        >>> s = SPC()
+        >>> s.set_662('1-2-3')
+        >>> s.spc
+        267
+        >>>
+        """
+        l = spc662.split('-')
+        if len(l) is not 3:
+            print("Error: Wrong format, should be like A-B-C")
+            return
+        else:
+            a, b, c = l
+            a = int(a)
+            b = int(b)   
+            c = int(c)
+            if a > 2**6 or b > 2**6 or c > 2**2:
+                print(("Error: %s does not look like Signaling Point Code Format 6-6-2 (max=%d-%d-%d, min=0-0-0), maybe it is ANSI (8-8-8)?\n" % (spc545, 2**6, 2**6, 2**2)))
+                return
+            self.spc = a*2**8 + b*2**2 + c
+
     def set_383(self, s):
         """
         >>> s = SPC()
@@ -170,6 +195,24 @@ class SPC():
         return "%d-%d-%d"%(a,b,c)
         # return ('-').join( ("%d"%a, "%d"%b, "%d"%c) )
 
+    def get_662(self):
+        pc = int(self.spc)
+        a = pc >> 8
+        b = (pc- a*2**8) >> 2
+        c = pc - a*2**8 - b*2**2
+        return a, b, c
+
+    def to_662(self):
+        """
+        >>> s = SPC()
+        >>> s.set_int(1234)
+        >>> s.to_662()
+        '4-52-2'
+        >>>
+        """
+        a, b, c = self.get_662()
+        return "%d-%d-%d"%(a,b,c)
+
     def to_hex(self):
         """
         >>> s = SPC()
@@ -191,21 +234,24 @@ class SPC():
         Hex Format  : H'4D2
         5-4-5 Format: 2-6-18
         3-8-3 Format: 0-154-2
+        6-6-2 Format: 4-52-2
         <BLANKLINE>
         >>>
         """
         if self.csv is True:
-            print(("%d,%s,%s,%s,%s" % (self.spc, 
+            print(("%d,%s,%s,%s,%s,%s" % (self.spc, 
                                     self.kind_string(), 
                                     self.to_hex(),
                                     self.to_545(), 
-                                    self.to_383())))
+                                    self.to_383(),
+                                    self.to_662())))
         else:
             print(("SPC Decimal : %d" % self.spc))
             print(("Format      : %s" % self.kind_string()))
             print(("Hex Format  : " + self.to_hex()))
             print(("5-4-5 Format: " + self.to_545()))
             print(("3-8-3 Format: " + self.to_383()))
+            print(("6-6-2 Format: " + self.to_662()))
             print("")
          
     def header(self):
@@ -213,7 +259,7 @@ class SPC():
         Displays header...
         """
         if self.csv is True:
-            return "SS7calc,SS7 Signaling Point Code calculator,by Philippe Langlois,http://www.p1security.com\nSPC Decimal,Format,Hex format,5-4-5 Format,3-8-3 Format"
+            return "SS7calc,SS7 Signaling Point Code calculator,by Philippe Langlois,http://www.p1security.com\nSPC Decimal,Format,Hex format,5-4-5 Format,3-8-3 Format,6-6-2 Format"
         else:
             return "SS7calc - SS7 Signaling Point Code calculator\nby Philippe Langlois - http://www.p1security.com\n"
 
@@ -228,7 +274,7 @@ def main(argv=None):
         argv = sys.argv
     try:
         try:
-            opts, args = getopt.getopt(argv[1:], "ho:vi:x:3:5:uar:c", ["help", "output=", "int=", "hex=", "383=", "545=", "itu", "ansi", "read=", "csv"])
+            opts, args = getopt.getopt(argv[1:], "ho:vi:x:3:5:6:uar:c", ["help", "output=", "int=", "hex=", "383=", "545=", "662=", "itu", "ansi", "read=", "csv"])
         except getopt.error as msg:
             raise Usage(msg)
 
@@ -252,6 +298,10 @@ def main(argv=None):
             #
             if option in ("-5", "--545"):
                 spc.set_545(value)
+                spc.set_itu()
+            # 
+            if option in ("-6", "--662"):
+                spc.set_662(value)
                 spc.set_itu()
             # 
             if option in ("-u", "--itu"):
